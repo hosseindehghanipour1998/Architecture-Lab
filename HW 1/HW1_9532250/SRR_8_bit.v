4d4c10b16c5226661a831ce9ff2bb8a0e673d12c
Hossein <hossein.dehghanipour1998@gmail.com>
2020-03-24 01:05:54 +0430

    Waiting for TA complementary description to carry on HW1_Part 2

---
+++ HW 1/HW1_9532250/SRR_8_bit.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:52:03 03/24/2020 
+// Design Name: 
+// Module Name:    SRR_8_bit 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+// Shift Right Register -> 8 bits , Serial
+module SRR_8_bit(input clk , input d_in );
+
+
+endmodule
