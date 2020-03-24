77a1c791dd3e0251b8b0de583e44fc15c73e14c2
Hossein <hossein.dehghanipour1998@gmail.com>
2020-03-24 21:01:13 +0430

    Implemented SRR -> Ready to test

--- HW 1/HW1_9532250/SRR_8_bit.v
+++ HW 1/HW1_9532250/SRR_8_bit.v
@@ -19,7 +19,18 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 // Shift Right Register -> 8 bits , Serial
-module SRR_8_bit(input clk , input d_in );
-
+module SRR_8_bit(clk ,d_in , out);
+	input clk ;
+	input d_in ;
+	output [7:0] out ;
+	
+	DFF dff0 (.d_in(d_in) , .clk(clk) , .q_out(out[7]) );
+	DFF dff1 (.d_in(out[7]) , .clk(clk) , .q_out(out[6]) );
+	DFF dff2 (.d_in(out[6]) , .clk(clk) , .q_out(out[5]) );
+	DFF dff3 (.d_in(out[5]) , .clk(clk) , .q_out(out[4]) );
+	DFF dff4 (.d_in(out[4]) , .clk(clk) , .q_out(out[3]) );
+	DFF dff5 (.d_in(out[3]) , .clk(clk) , .q_out(out[2]) );
+	DFF dff6 (.d_in(out[2]) , .clk(clk) , .q_out(out[1]) );
+	DFF dff7 (.d_in(out[1]) , .clk(clk) , .q_out(out[0]) );
 
 endmodule
