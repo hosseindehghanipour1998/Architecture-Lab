fedf29f23654b6bf03785b4801c1604f3a1f9390
Hossein <hossein.dehghanipour1998@gmail.com>
2020-04-02 03:31:35 +0430

    HW 2 -> implemented ALU
    implemented ALU module with Nested Tristate Buffers -> waiting to be tested

--- HW 2/HW2/ALU.v
+++ HW 2/HW2/ALU.v
@@ -18,7 +18,26 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module ALU(input_A, input_B, control_bits, output_O);
+module ALU(input_A, input_B, control_bits, output_O , zero);
+input [15:0] input_A ;
+input [15:0] input_B ;
+input [2:0] control_bits ;
+output [15:0]  output_O ;
+output zero ;
+
+assign output_O = control_bits[0] ? 
+							(control_bits[1] ?  // if control_bits[0] == 1
+										(input_A | input_B) : 
+										(input_A - input_B)  )
+						 : (control_bits[1] ? // control_bits[0] == 0 
+									(input_A ^ input_B): // control_bits[1] == 1 
+									(control_bits[2] ? // control_bits[1] == 0
+												((input_A > input_B) ? 16'b0000000000000001 : 16'b0000000000000000) : (input_A + input_B)
+									
+									 ) 
+							
+							) ; 
+
 
 
 endmodule
