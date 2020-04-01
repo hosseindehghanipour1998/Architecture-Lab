8d6cf8c488011241d6f36c8197cfb1c3ba2debde
Hossein <hossein.dehghanipour1998@gmail.com>
2020-04-02 03:06:03 +0430

    HW 2 -> Added Primary Files
    ALU.v

---
+++ HW 2/HW2/ALU.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    03:03:03 04/02/2020 
+// Design Name: 
+// Module Name:    ALU 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ALU(input_A, input_B, control_bits, output_O);
+
+
+endmodule
