4d188a52d4414450766bea9f0677adf713885812
Hossein <hossein.dehghanipour1998@gmail.com>
2020-08-18 10:35:46 +0430

    Implemented Execute_Stage Module

--- MIPS/Codes/Execute_Stage.v
+++ MIPS/Codes/Execute_Stage.v
@@ -24,10 +24,42 @@ module Execute_Stage(
 	input [15:0] 	in_Immediate,
 	input 		 	in_ALUSrc,
 	input	[1:0]  	in_ALUOp,
-	
+	input [15:0]	in_PC_plus_two,
+	//outputs
 	output [15:0]	O_ALUResult,
 	output 			O_Zero,
+	output [15:0]	O_addResult 
     );
+	 
+	 wire [15:0] outMux_inALU ;
+	 wire [2:0] outALUControl_inALU ;
+	 
+	 ALU ALU (
+		 .input_A(in_Read_Data_1), 
+		 .input_B(outMux_inALU), 
+		 .ALU_Control(outALUControl_inALU), 
+		 .Result(O_ALUResult), 
+		 .Zero(O_Zero)
+    );
+
+	ALU_Control ALU_Control (
+		 .ALUOp(in_ALUOp), 
+		 .Function(in_Immediate[3:0]), 
+		 .ALUcnt(outALUControl_inALU)
+	 );
+	 
+	 Mux Execute_Mux (
+    .input0(in_Read_Data_2), 
+    .input1(in_Immediate), 
+    .out(outMux_inALU), 
+    .select(in_ALUSrc)
+    );
+
+	
+	assign O_addResult = ( in_Immediate << 1 ) + (in_PC_plus_two) ;
+
+
+
 
 
 endmodule
