afffb266f9fdb431bd8411401cc8dd5fe88623f2
Hossein <hossein.dehghanipour1998@gmail.com>
2020-08-18 10:39:14 +0430

    Deleted IE_MEM Module

---
+++ MIPS/Codes/EX_MEM.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    10:38:27 08/18/2020 
+// Design Name: 
+// Module Name:    IE_MEM 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module IE_MEM(
+    );
+
+
+endmodule
